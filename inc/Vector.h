dd9484a24fb51a01659437ffadba396caa08727c
Diego Macario <diego.macario5@gmail.com>
2017-11-05 20:41:47 -0500

    Update "Possible Improvements" comment.

--- inc/Vector.h
+++ inc/Vector.h
@@ -9,8 +9,8 @@ Description:
 
 Possible improvements:
 
-   - There are 3 versions of the dot() function. Two are provided here and one inside Point.h.
-     A template could be created to unify them and reduce code duplication.
+   - There are 3 versions of the dot() function. Two are provided here, and two inside Point.h.
+     A template could be created to unify them, reducing code duplication.
 */
 
 class Point;
